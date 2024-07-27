649bf2f2d33d07d9c408416523097191757fd794
dgz <dgz@dgz.me>
2024-07-27 14:42:38 -0400

    Start work on configuration and basic functions
    The architectural ideas here are simple: the core is not responsible
    for the configuration file, nor does it have any knowledge of a
    configuration file.
    
    The frontend will parse its own configuration file, tell the core what
    it found, and if the core has a problem with the values of a given
    configuration, it will report that issue back to the frontend.
    
    Signed-off-by: dgz <dgz@dgz.me>

---
+++ src/core/src/public/core/conf.h
@@ -0,0 +1,58 @@
+// Copyright 2024 dgz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+/// @file conf.h Defines the configuration structure of an IRC server instance.
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#include <stdbool.h>
+#include <stddef.h>
+#include "types.h"
+
+// clang-format off
+
+#define IRC_CONF_LISTENER_NUM_MAX	(10)
+#define IRC_CONF_LISTENER_HOST_LEN_MAX	(32)
+
+// clang-format on
+
+struct irc_conf_listener {
+	char host[IRC_CONF_LISTENER_HOST_LEN_MAX];
+	uint port;
+};
+
+struct irc_conf {
+	struct {
+		struct irc_conf_listener entries[IRC_CONF_LISTENER_NUM_MAX];
+		size_t num_entries;
+	} listeners;
+};
+
+bool irc_conf_listener_add(struct irc_conf *conf,
+			   struct irc_conf_listener *listener,
+			   size_t host_str_len);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus

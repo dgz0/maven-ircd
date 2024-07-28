14613a1aeaa9620b9b92b9f7d4c1c3014c694d91
dgz <dgz@dgz.me>
2024-07-28 15:49:54 -0400

    Fix edge cases during IPv4 address detection
    Signed-off-by: dgz <dgz@dgz.me>

--- src/core/src/private/util.c
+++ src/core/src/private/util.c
@@ -24,7 +24,7 @@
 
 // clang-format off
 
-#define IPV4_NUM_PARTS		(3)
+#define IPV4_NUM_DOTS		(3)
 #define IPV4_LEN_MIN		(7)
 #define IPV4_LEN_MAX		(15)
 
@@ -36,9 +36,17 @@ bool is_ipv4(const char *const str, const size_t len)
 		return false;
 	}
 
-	uint parts = 0;
+	// How many dots have we seen?
+	uint num_dots = 0;
+
+	// How many digits have we seen in the group currently being processed
+	// so far?
 	uint num_digits_seen = 0;
 
+	// The first seen digit of the group currently being processed.
+	char first_digit = '\0';
+
+	// The last digit we processed (NOT the last digit of the group).
 	char last_digit = '\0';
 
 	for (size_t i = 0; i < len; ++i) {
@@ -47,25 +55,39 @@ bool is_ipv4(const char *const str, const size_t len)
 				return false;
 			}
 
-			parts++;
+			num_dots++;
 
-			if (parts > IPV4_NUM_PARTS) {
+			if (num_dots > IPV4_NUM_DOTS) {
 				return false;
 			}
+
+			first_digit = '\0';
+			last_digit = '\0';
+
 			num_digits_seen = 0;
 		} else if (isdigit(str[i])) {
 			switch (num_digits_seen) {
 			case 0:
-				// We haven't seen any other digits yet, but
-				// since we know this is a digit ranged from
-				// 0-9, we don't care.
+				// The digit we're currently processing is the
+				// first digit in the group. Since we know the
+				// range of an IP address bit field is 0 - 255,
+				// we can inspect the first digit and bail out
+				// if it isn't between 0 to 2.
+				if (str[i] > '2') {
+					return false;
+				}
+
+				first_digit = str[i];
 				break;
 
 			case 1:
 				if (last_digit == '0') {
 					// We have another digit, but the first
 					// one we saw was zero; leading zeros
-					// are not allowed.
+					// are not allowed, but would be okay if
+					// we didn't have another digit. This is
+					// why we don't check for zero in the
+					// previous case.
 					//
 					// "192.168.01.100"
 					//          ^ error because next
@@ -84,12 +106,10 @@ bool is_ipv4(const char *const str, const size_t len)
 				break;
 
 			case 2:
-				if ((last_digit == '5') && (str[i] > '5')) {
-					// Each field of an IP address is ranged
-					// from 000 to 255.
-					//
-					// "192.257.0.0"
-					//        ^ (7 > 5), not valid
+				if ((first_digit == '2') &&
+				    (last_digit == '5') && (str[i] > '5')) {
+					// "192.256.12.12"
+					//      ^ ^ (6 > 2), not valid
 					return false;
 				}
 				break;
@@ -105,8 +125,7 @@ bool is_ipv4(const char *const str, const size_t len)
 		} else {
 			// Not a number or a dot.
 			//
-			// "192.168.A.B"
-			//          ^ ^ invalid characters
+			// "192.168.A.B" <- invalid
 			return false;
 		}
 	}
